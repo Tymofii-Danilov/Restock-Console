dd0418ddbaf0d5c8a9fd0e7d5e285ab563f64c51
Tymofii Danilov <timadanilov2002@gmail.com>
2026-07-30 20:19:31 +0200

    fix sticky header and arrow symbols in sorting filters

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.dummyjson.com' },
-      { protocol: 'https', hostname: 'dummyjson.com' }
-    ]
-  }
+      { protocol: 'https', hostname: 'dummyjson.com' },
+    ],
+  },
+  allowedDevOrigins: ['192.168.0.102'],
 };
 
 export default nextConfig;
